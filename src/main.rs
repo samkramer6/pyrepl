5f47e24b362b398e67a834ce7002b30517239a51
samkramer6 <samkramer6@vt.edu>
2024-06-10 09:56:56 -0500

    Updating to remove multiline

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-use rustyline::highlight::MatchingBracketHighlighter;
-use rustyline::validate::MatchingBracketValidator;
-use rustyline::completion::Completer;
-use rustyline
-
-
 use rustyline::error::ReadlineError;
-use crate::py_evaluate::evaluate_code;
 use rustyline::DefaultEditor;
+
+use crate::py_evaluate::evaluate_code;
 
 mod repl_setup;
 mod py_evaluate;
@@ -14,29 +9,13 @@ mod py_evaluate;
 #[allow(dead_code)]
 #[allow(unused_imports)]
 
-struct InputValidator {
-    #[rustyline(Validator)]
-    brackets: MatchingBracketValidator,
-    #[rustyline(Highlighter)]
-    highlighter: MatchingBracketHighlighter,
-}
-
 fn main() {
 
     repl_setup::welcome_message();    
     let mut entire_string = String::new().to_owned();
 
-    let validator = InputValidator{
-        brackets: MatchingBracketValidator::new(),
-        highlighter: MatchingBracketHighlighter::new(),
-    };
     let mut editor = DefaultEditor::new();
-    editor.expect("Could Not Set Helper").set_helper(Some(validator));
-    // editor.bind_sequence(
-    //     KeyEvent(KeyCode::Char('N'), Modifiers::CTRL),
-    //     EventHandler::Simple(Cmd::Newline),
-    // );
-
+    
     'outer: loop {
            
         // Rustyline read
